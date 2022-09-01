9a78694c61e4010bdc28a464c8d93073c7f23232
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-09-01 13:14:24 +0530

    Email Varification

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../libs/prisma";
 
@@ -9,6 +10,17 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST || "",
+        port: process.env.EMAIL_SERVER_PORT || "",
+        auth: {
+          user: process.env.EMAIL_SERVER_USER || "",
+          pass: process.env.EMAIL_SERVER_PASSWORD || "",
+        },
+      },
+      from: process.env.EMAIL_FROM || "",
     }),
   ],
   secret: process.env.AUTH_SECRET,
