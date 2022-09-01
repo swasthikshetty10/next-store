464f8520f90fc724ee2f388480f5c145555f586e
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-09-01 13:32:12 +0530

    passless email login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../libs/prisma";
+import { sendVerificationRequest } from "../../../utils/email";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -14,13 +15,14 @@ export const authOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST || "",
-        port: process.env.EMAIL_SERVER_PORT || "",
+        port: Number(process.env.EMAIL_EMAIL_SERVER_PORT),
         auth: {
           user: process.env.EMAIL_SERVER_USER || "",
           pass: process.env.EMAIL_SERVER_PASSWORD || "",
         },
       },
       from: process.env.EMAIL_FROM || "",
+      sendVerificationRequest,
     }),
   ],
   secret: process.env.AUTH_SECRET,
